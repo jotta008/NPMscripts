7cc4f5069564d1893c2cd957dcbf896429cada7e
Jose Cirer <josecirer@gmail.com>
2020-11-23 18:45:00 -0300

    Gulp Part 1

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,26 @@
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	browserSync = require('browser-sync')
+
 gulp.task('sass', function () {
 	return gulp
 		.src('./css/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./css'))
 })
+
 gulp.task('sass:watch', function () {
 	gulp.watch('./css/*.scss', ['sass'])
 })
+
 gulp.task('browser-sync', function () {
-	var files = ['./*.html', './css/*.css', './js/*.js', '/img/*.{png,jpg,gif}']
+	var files = [
+		'./*.html',
+		'./css/*.css',
+		'./img/*.{png,jpg,gif}',
+		'./js/*.js',
+	]
+
 	browserSync.init(files, {
 		server: {
 			baseDir: './',
@@ -20,9 +29,7 @@ gulp.task('browser-sync', function () {
 	})
 })
 
-gulp.task('default', [
-	'browser-sync',
-	function () {
-		gulp.start('sass:watch')
-	},
-])
+// Default task
+gulp.task('default', ['browser-sync'], function () {
+	gulp.start('sass:watch')
+})
